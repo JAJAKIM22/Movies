1ee03818b425e8344e05d88d9c835c55f5b4ec2a
Peter <peterjajakim@gmail.com>
2023-02-15 17:57:12 +0300

    ADDED SIGNUP AND SIGNOUT

--- components/Header.js
+++ components/Header.js
@@ -8,9 +8,13 @@ import {
     SearchIcon,
     UserIcon,
 } from "@heroicons/react/outline"
+import { signIn, signOut, useSession } from "next-auth/react"
 
 function Header() {
-  return (
+  const { data: session, status } = useSession()
+
+  if (status === "authenticated") {
+    return (
     <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
      <div className="flex  flex-grow justify-evenly max-w-2xl ">
         <HeaderItem title="HOME" Icon={HomeIcon} />
@@ -18,13 +22,28 @@ function Header() {
         <HeaderItem title="VERIFIED" Icon={BadgeCheckIcon} />
         <HeaderItem title="COLLECTIONS" Icon={CollectionIcon} />
         <HeaderItem title="SEARCH" Icon={SearchIcon} />
-        <HeaderItem title="ACCOUNT" Icon={UserIcon} />
+        <HeaderItem  title={session.user.name} Icon={UserIcon} />
         </div>   
    {/* <Image  className="object-contain"
    src="https://links.papareact.com/ua6"
    width={200}
    height={100}
     /> */}
+    <div>
+      {/* <p> {session ? `${session.user.name}` : "signin"}  </p> */}
+      
+    <button onClick={signOut}  className="rounded-md bg-gray-100 text-black  hover:text-black-200  w-40 text-lg active:text-red-500">Sign Out</button>
+    </div>
+    </header>
+    )
+  }
+  
+  
+  return (
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+     <div> 
+    <button onClick={signIn}  className="rounded-md bg-gray-100 text-black  hover:text-black-200  w-40 text-lg active:text-red-500">Sign in</button>
+    </div>
     </header>
   )
 }
---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBx6Ddow1LvCnW1Y38ZpnDkbe2idQ8slgA",
+    authDomain: "movies-91c3e.firebaseapp.com",
+    projectId: "movies-91c3e",
+    storageBucket: "movies-91c3e.appspot.com",
+    messagingSenderId: "1077755882206",
+    appId: "1:1077755882206:web:498060aae73c5cd4cf304f"
+  };
